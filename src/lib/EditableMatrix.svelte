c21e8731be191fc44d4a9a896c424c9d5ad5a0ac
ThePotatoArchivist <josiahfu@gmail.com>
2025-02-13 16:48:48 -0800

    Add deploy action

--- src/lib/EditableMatrix.svelte
+++ src/lib/EditableMatrix.svelte
@@ -16,16 +16,4 @@
         width: 1.5em;
         text-align: center;
     }
-
-    /* Chrome, Safari, Edge, Opera */
-    input::-webkit-outer-spin-button,
-    input::-webkit-inner-spin-button {
-        -webkit-appearance: none;
-        margin: 0;
-    }
-
-    /* Firefox */
-    input[type=number] {
-        -moz-appearance: textfield;
-    }
 </style>
