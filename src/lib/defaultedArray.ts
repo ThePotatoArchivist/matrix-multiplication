e2cf37e3f500a9312bff6bd04470539555779704
ThePotatoArchivist <josiahfu@gmail.com>
2025-04-07 17:14:37 -0700

    Update title

--- src/lib/defaultedArray.ts
+++ src/lib/defaultedArray.ts
@@ -1,10 +1,12 @@
 function defaultedArray<T>(array: T[], defaultValue: () => T) {
     return new Proxy(array, {
         get(target, p, receiver) {
-            if (typeof p === 'string' && !isNaN(parseInt(p)) && !(p in target)) {
-                target[p as unknown as number] = defaultValue()
-            }
-            return Reflect.get(target, p, receiver)
+            if (typeof p !== 'string' || isNaN(parseInt(p)) || p in target)
+                return Reflect.get(target, p, receiver)
+            
+            const value = defaultValue()
+            Reflect.set(target, p, value, receiver)
+            return value
         },
     })
 }
